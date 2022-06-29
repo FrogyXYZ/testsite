07efd905cd89f18840338769ba193d073ca103e2
FrogyXYZ` <dmbl@list.ru>
2022-06-29 22:05:49 +0300

    lesson 12

---
+++ mysite/news/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name = 'наименование')
+    content = models.TextField(blank=True, verbose_name = 'контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name = 'дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name = 'обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name = 'фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name = 'опубликовано')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name ='Новость'
+        verbose_name_plural= 'Новости'
+        ordering = ['-created_at', 'title']
+
+
+
+
+
